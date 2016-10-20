cbdf6a34b36d5a1a944a290d6bee7b3c6f0e70bc
lbraun2 <lbraun2@masonlive.gmu.edu>
2016-10-19 23:19:18 -0400

    attempted map test

--- public/CoffeeSpec.js
+++ public/CoffeeSpec.js
@@ -1,3 +1,4 @@
+
 describe("Homepage", function () {
     var TestUtils = React.addons.TestUtils;
     var component, element, renderedDOM;
@@ -15,4 +16,35 @@ describe("Homepage", function () {
         TestUtils.findRenderedComponentWithType(component,TodoList);
     }).not.toThrow();
 });
+});
+describe("Map", function() {
+
+		var map, places, status_return;
+		beforeEach(function(){
+           map = new google.maps.Map(document.getElementById('map'), {
+             zoom: 3,
+             center:  {lat: 37.1, lng: -95.7},
+             mapTypeControl: false,
+             panControl: false,
+             zoomControl: false,
+             streetViewControl: false
+           });
+		   
+		   places = new google.maps.places.PlacesService(map);
+		   
+		   var search = {
+             bounds: map.getBounds(),
+             types: ['cafe']
+           };
+		   
+		   places.nearbySearch(search, function(results, status) {
+		     status_return = status;
+		   }
+		   });
+	   it("has a OK return status"), function(){
+	     expect(status_return).toEqual(google.maps.places.PlacesServiceStatus.OK);
+	   }
+		   
+		   
+
 });
