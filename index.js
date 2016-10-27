8d36e90ebb1f9c2c2a55c9e43d36df2d5744ca76
enkonya <enkonya@gmu.edu>
2016-10-27 09:00:23 -0400

    HW6

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 firebase.initializeApp({
     serviceAccount: "privkey.json",
-    databaseURL: "https://swe432lecture12.firebaseio.com"
+    databaseURL: "https://coffeestop-820c3.firebaseio.com"
 });
-var fireRef = firebase.database().ref('todos');
+var fireRef = firebase.database().ref('coffeestop');
 
 var port = process.env.PORT || 3000;
 
-app.post('/newTodo', function (req, res) {
+app.post('/newUser', function (req, res) {
     console.log("New req");
     console.log("Client wants to create todo: '" + req.body.todoText + "'");
     fireRef.push({"text": req.body.todoText}, function () {
@@ -24,36 +24,12 @@ app.post('/newTodo', function (req, res) {
         res.status(403);
     });
 });
-app.post('/editTodo', function (req, res) {
-    console.log("Client wants to update todo: '" +req.body.key+ " To " + req.body.todoText + "'");
-    if(req.body.todoText.toLowerCase().includes("lasagna"))
-        res.status(403);
-    else
-        fireRef.child(req.body.key).set({"text": req.body.todoText}, function () {
-            res.send("OK!");
-        }).catch(function(){
-            res.status(403);
-        });
+
 });
-app.post('/deleteTodo', function (req, res) {
-    console.log("Client wants to delete todo: '" +req.body.key);
-    fireRef.child(req.body.key).once("value", function(item){
-        if(item.val().text.toLowerCase().includes("lasagna"))
-            res.status(403);
-        else
-        {
-            fireRef.child(req.body.key).remove();
-            res.send("OK!");
-        }
-    }).catch(function(){
-        res.status(403);
-    });
-});
-app.get('/emptyHtml.html', function (req, res) {
-    console.log("Requested empty html");
+app.get('/index.html', function (req, res) {
+    console.log("Requested Homepage");
     res.send("OK!");
 });
-
 
 app.use(express.static('public'));
 
